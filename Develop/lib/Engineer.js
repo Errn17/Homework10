380c1a44cc8392423809ca559ecccb077b84e740
Aaron <aaronkh19@gmail.com>
2020-06-06 11:38:47 -0600

    started the engineer class

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,6 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+//Requiring the employee page to use the constructor function that was setup
+const Employee = require("./Employee");
+
+//class for the Engineer, will also be using the emplyee one to avoid duplicating the constructor parts. Will utilize the "super" keyword
